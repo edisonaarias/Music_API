bb4f48ff10a9ee193281ae4c16bf2bddf854e0df
unknown <edisonalexanderarias@gmail.com>
2022-06-17 09:17:05 -0700

    Changed database setting + created music app.

---
+++ music_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^vz-q=_(th^czk$8_3h&uh0$x)lb!c9&f_*24$spdd69+!omqb'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '555429ea',
+    }
+}
